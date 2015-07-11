e8cf3918178afbb11add417bea21b52948b10366
myrtletree33 <me@joeltong.org>
2015-07-11 14:31:57 +0800

    committing new file

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -8,12 +8,22 @@ Template.Home.events({
 /* Home: Helpers */
 /*****************************************************************************/
 Template.Home.helpers({
+  currentTweet: function() {
+    return Session.get('currentTweet');
+  }
 });
 
 /*****************************************************************************/
 /* Home: Lifecycle Hooks */
 /*****************************************************************************/
 Template.Home.created = function () {
+  console.log('got here!');
+  setInterval(function(){
+     Session.set('currentTweet',
+     Tweets
+     .find({}, {sort: {timestamp_ms : -1}, limit: 1 })
+       .fetch()[0]);
+  }, 2000);
 };
 
 Template.Home.rendered = function () {
--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -1,2 +1,32 @@
-Meteor.startup(function () {
+Meteor.startup(function() {
+  loadTwitter();
 });
+
+function loadTwitter() {
+  var twitter = new Twitter({
+    consumer_key: Meteor.settings.twitter_consumer_key,
+    consumer_secret: Meteor.settings.twitter_consumer_secret,
+    access_token_key: Meteor.settings.twitter_access_token,
+    access_token_secret: Meteor.settings.twitter_access_token_secret
+  });
+
+  twitter.stream('statuses/filter', {
+    track: 'singapore'
+  }, function(stream) {
+    stream.on('data', function(tweet) {
+      console.log(tweet);
+      if (Tweets.find().count() > 100) {
+        Tweets.remove({});
+      }
+      Tweets.insert({
+        screen_name: tweet.user.screen_name,
+        timestamp_ms: tweet.timestamp_ms,
+        text: tweet.text
+      });
+    });
+
+    stream.on('error', function(error) {
+      throw error;
+    });
+  });
+};
