b25d74be013b04673f2039ef4043be0d4ae044e0
myrtletree33 <me@joeltong.org>
2015-07-22 23:39:06 +0800

    committing changes

--- app/client/templates/timeline/timeline.js
+++ app/client/templates/timeline/timeline.js
@@ -13,7 +13,7 @@ Template.Timeline.helpers({});
 /*****************************************************************************/
 Template.Timeline.created = function() {};
 
-// Template.Timeline.rendered = function() {
+Template.Timeline.rendered = function() {
   $(document).ready(function() {
 
     var timelineBlocks = $('.cd-timeline-block'),
@@ -40,7 +40,7 @@ Template.Timeline.created = function() {};
     function showBlocks(blocks, offset) {
       blocks.each(function() {
         ($(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).find('.cd-timeline-img').hasClass('is-hidden')) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-      // });
+      });
     }
   });
 };
