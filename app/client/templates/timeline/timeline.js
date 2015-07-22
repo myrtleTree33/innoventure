6c81489dec5b35b250040e69ff2fcb2ae2509bbb
myrtletree33 <me@joeltong.org>
2015-07-22 23:35:32 +0800

    added FAQ elements and updated timeline

--- app/client/templates/timeline/timeline.js
+++ app/client/templates/timeline/timeline.js
@@ -14,30 +14,35 @@ Template.Timeline.helpers({});
 Template.Timeline.created = function() {};
 
 Template.Timeline.rendered = function() {
-  var timelineBlocks = $('.cd-timeline-block'),
-		offset = 0.8;
+  $(document).ready(function() {
 
-	//hide timeline blocks which are outside the viewport
-	hideBlocks(timelineBlocks, offset);
+    var timelineBlocks = $('.cd-timeline-block'),
+      offset = 0.8;
 
-	//on scolling, show/animate timeline blocks when enter the viewport
-	$(window).on('scroll', function(){
-		(!window.requestAnimationFrame)
-			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
-			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
-	});
+    //hide timeline blocks which are outside the viewport
+    hideBlocks(timelineBlocks, offset);
 
-	function hideBlocks(blocks, offset) {
-		blocks.each(function(){
-			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-		});
-	}
+    //on scolling, show/animate timeline blocks when enter the viewport
+    $(window).on('scroll', function() {
+      (!window.requestAnimationFrame) ? setTimeout(function() {
+        showBlocks(timelineBlocks, offset);
+      }, 100): window.requestAnimationFrame(function() {
+        showBlocks(timelineBlocks, offset);
+      });
+    });
 
-	function showBlocks(blocks, offset) {
-		blocks.each(function(){
-			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-		});
-	}
+    function hideBlocks(blocks, offset) {
+      blocks.each(function() {
+        ($(this).offset().top > $(window).scrollTop() + $(window).height() * offset) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+      });
+    }
+
+    function showBlocks(blocks, offset) {
+      blocks.each(function() {
+        ($(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).find('.cd-timeline-img').hasClass('is-hidden')) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+      });
+    }
+  });
 };
 
 Template.Timeline.destroyed = function() {};
