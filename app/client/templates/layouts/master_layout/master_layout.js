8efccba622cb7d88b0a05abd169beb6ddd49a58a
myrtletree33 <me@joeltong.org>
2015-08-02 00:24:45 +0800

    fixed navigation problem where mobile nav fails to return to original position upon navigate to new page

--- app/client/templates/layouts/master_layout/master_layout.js
+++ app/client/templates/layouts/master_layout/master_layout.js
@@ -1,5 +1,19 @@
-Template.MasterLayout.helpers({
-});
+Template.MasterLayout.helpers({});
 
-Template.MasterLayout.events({
-});
+Template.MasterLayout.events({});
+
+Template.MasterLayout.rendered = function() {
+  $(document).ready(function() {
+
+    // hack to push mobile top menu to original closed
+    // position upon clicking
+    $('nav.top-bar > .top-bar-section > .right').on('click', function(e) {
+      if ($(e.target).parent().has('ul').length === 0) {
+        $('nav.top-bar').removeClass('expanded').animate({
+        });
+      }
+    });
+
+
+  });
+};
